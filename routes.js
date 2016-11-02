394d9fd80f553746e4b71e0dda851c481fb70c11
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-11-02 18:56:53 -0400

    wrote the logic for user registration

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 var express = require("express");
 
+// require the passport module
+var passport = require("passport");
+
 // import the User model
 var User = require("./models/user");
 
@@ -24,5 +27,33 @@ router.get("/", function(req, res, next) {
 		res.render("index", { users: users });
 	});
 });
+
+router.get("/signup", function(req, res) {
+	res.render("signup");
+});
+
+router.post("/signup", function(req, res, next) {
+	var username = req.body.username;
+	var password = req.body.password;
+
+	User.findOne({ username: username }, function(err, user) {
+		if (err) { return next(err); }
+		if (user) {
+			req.flash("error" "User already exists.");
+			return res.redirect("/signup");
+		}
+
+		var newUser = new User({
+			username: username,
+			password: password
+		});
+		newUser.save(next);
+
+	});
+}, passport.authenticate("login", {
+	successRedirect: "/",
+	failureRedirect: "/signup",
+	failureFlash: true
+}));
 
 module.exports = router;
